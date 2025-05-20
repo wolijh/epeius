fc512a14ab46d8de35210e5196ae470ecff49339
wolijh <160990627+wolijh@users.noreply.github.com>
2025-05-20 20:26:38 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from "cloudflare:sockets";
 
-let password = '';
+let password = 'kL-fQ';
 let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
